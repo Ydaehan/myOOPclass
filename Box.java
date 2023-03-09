eb7d7e10aa4ff64e8f92fad7688298977fa2f9be
Ydaehan <daehan821@naver.com>
2023-03-09 11:06:32 +0900

    '23.03.03.(금)

---
+++ Box.java
@@ -0,0 +1,5 @@
+public class Box {
+  int width;
+  int length;
+  int height;
+}
---
+++ Boxtest.java
@@ -0,0 +1,11 @@
+public class Boxtest {
+  public static void main(String[] args) {
+    
+    Box b;
+    b = new Box();
+    b.width = 20;
+    b.length = 20;
+    b.height = 30;
+    System.out.println("상자의 가로, 세로, 높이는 " + b.width + ", " + b.length+ ", " + b.height + "입니다.");
+  }
+}
